ca3e388715aaba7b835434ff4cd8df4ccc1f2ce9
Mulliganaceous <mulliganaceous@outlook.com>
2022-01-09 16:24:17 -0500

    Support for Linux. I am a Linux user

--- Merlin/run.sh
+++ Merlin/run.sh
@@ -3,11 +3,11 @@ if [ -z "$1" ]; then
     exit 1
 fi
 tmux new -d -s 'deerhunt'
-tmux send-keys "python runServer.py --render $1" C-m
+tmux send-keys "python3 runServer.py --render $1" C-m
 tmux new-window
-tmux send-keys "sleep 3 && python runClient.py $(hostname) $1" C-m
+tmux send-keys "sleep 3 && python3 runClient.py $(hostname) $1" C-m
 tmux split -h
-tmux send-keys "sleep 1 && python runClient.py $(hostname) $1" C-m
+tmux send-keys "sleep 1 && python3 runClient.py $(hostname) $1" C-m
 tmux next-window
 
 tmux a
