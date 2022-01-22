1aab0ac757a9f05683a52c4b889c40bdb581a723
HassanA01 <anikeboss@gmail.com>
2022-01-21 19:00:35 -0500

    updated tick method

--- Merlin/client/grid_player.py
+++ Merlin/client/grid_player.py
@@ -53,41 +53,59 @@ class GridPlayer:
                 pos = -1
             c = your_flag['y']
             r = your_flag['x']
-            self.guard.append(StandbyGaurd(r + 1, c))
-            self.guard.append(StandbyGaurd(r - 1, c))
-            self.guard.append(StandbyGaurd(r, c + pos))
+            self.guard.append(StandbyGaurd((r + 1, c), 4))
+            self.guard.append(StandbyGaurd((r - 1, c), 4))
+            self.guard.append(StandbyGaurd((r, c + pos), 4))
+            c = enemy_flag["y"]
+            r = enemy_flag["x"]
+            self.guard.append(StandbyGaurd((r, c), 4))
+            self.guard.append(StandbyGaurd((r, c), 4))
+            self.guard.append(StandbyGaurd((r, c), 4))
+            self.guard.append(StandbyGaurd((r, c), 4))
+            self.guard.append(StandbyGaurd((r, c), 4))
+
             for i in self.enemy_resources:
                 self.guard.append(StandbyGaurd(i, 2))
+
+            self.guard.extend([StandbyGaurd(i, 2) for i in get_prime_coordinates(game_map, self.locked, (your_flag["x"], your_flag["y"]), self.avail_resources)])
             # self.guard.append(StandbyGaurd((r, c), 2))
             # self.guard.append(StandbyGaurd((your_flag['x'], your_flag['y']),2))
+            for i in range(35):
+                self.buy.append(Buy(Units.KNIGHT))
             self.buy.append(Buy(Units.KNIGHT))
             self.buy.append(Buy(Units.KNIGHT))
+            self.buy.append(Buy(Units.WORKER))
             self.buy.append(Buy(Units.KNIGHT))
             self.buy.append(Buy(Units.KNIGHT))
+            self.buy.append(Buy(Units.WORKER))
             self.buy.append(Buy(Units.KNIGHT))
-            self.buy.append(Buy(Units.KNIGHT))
-            self.buy.append(Buy(Units.KNIGHT))
+
             # self.buy.append(Buy(Units.KNIGHT))
             # self.buy.append(Buy(Units.KNIGHT))
-            # self.buy.append(Buy(Units.KNIGHT))
-            # self.buy.append(Buy(Units.KNIGHT))
+            # # self.buy.append(Buy(Units.KNIGHT))
+            # # self.buy.append(Buy(Units.KNIGHT))
+            # # self.buy.append(Buy(Units.KNIGHT))
+            # # self.buy.append(Buy(Units.KNIGHT))
             self.buy.append(Buy(Units.WORKER))
             self.buy.append(Buy(Units.WORKER))
             self.buy.append(Buy(Units.WORKER))
             self.buy.append(Buy(Units.WORKER))
-            self.buy.append(Buy(Units.WORKER))
-            self.buy.append(Buy(Units.WORKER))
-            self.buy.append(Buy(Units.WORKER))
-            self.buy.append(Buy(Units.WORKER))
+            # self.buy.append(Buy(Units.WORKER))
+            # self.buy.append(Buy(Units.WORKER))
+            # self.buy.append(Buy(Units.SCOUT))
         print("turn taken: ", self.count)
         add2 = {Units.WORKER: [], Units.ARCHER: [], Units.KNIGHT: [], Units.SCOUT: []}
         add, delete = self.adding_tags(your_units)
+        for i in delete:
+            self.buy.append(Buy(i.type))
         for i in add:
             self.protect[i.get_id()] = []
             add2[your_units.get_unit(i.get_id()).type].append(i)
         locked = self.update_locked(enemy_units)
         # locked = copy.deepcopy(self.locked)
         self.initialize_decision(your_units)
+        # for i in add2[Units.SCOUT]:
+        #     i.add_decision(Scouting(False))
         for i in add2[Units.WORKER]:
             print(i)
 
@@ -110,8 +128,8 @@ class GridPlayer:
                 if len(self.protect[j.get_id()]) < 4:
                     for x, y in ((c, r + 2), (c, r - 2), (c + 2, r), (c - 2, r)):
                         if is_within_map(locked, x, y) and (x, y) not in self.protect[j.get_id()]:
-                            self.guard.append(StandbyGaurd((x, y), 2))
-                            j.add_decision(Buy(Units.KNIGHT))
+                            #self.guard.append(StandbyGaurd((x, y), 2))
+                            #j.add_decision(Buy(Units.KNIGHT))
                             self.protect[j.get_id()].append((x, y))
                             break
 
@@ -187,7 +205,7 @@ class GridPlayer:
         temp_dict = set(your_units.get_all_unit_ids()) - set(self.id_dict.keys())
         lst1 = list()  # New units (on the board)
         for i in temp_dict:
-            game_unit = GameUnit(i)
+            game_unit = GameUnit(i, your_units.get_unit(i).type)
             self.id_dict[i] = game_unit
 
             lst1.append(game_unit)
@@ -195,7 +213,8 @@ class GridPlayer:
         temp = set(self.id_dict) - set(your_units.get_all_unit_ids())
         for i in temp:
             lst.append(self.id_dict.pop(i))
-
+            if i.type == Units.WORKER:
+                self.avail_resources[i.id] = -1
         return lst1, lst
 
     def initialize_tags(self, your_units: List[Unit]) -> None:
@@ -241,7 +260,8 @@ class Decision:
 
 class GameUnit:
 
-    def __init__(self, idd: int):
+    def __init__(self, idd: int, type):
+        self.type = type
         self.id = idd
         self.decision = Q()
         self.current = None
@@ -671,8 +691,30 @@ class Q:
 
 class Scouting(Decision):
 
-    def __init__(self, game_map, locked):
+    def __init__(self, moving):
         super().__init__()
+        self.moving = moving
+        self.path = []
+
+    def are_u_done(self, unit: Unit, **kwargs) -> bool:
+        return False
+
+    def next_move(self, unit: Unit, **kwargs) -> Tuple[Move, int]:
+        """
+
+        :param unit:
+        :param Kwargs:
+        :return:
+        """
+        if not self.moving:
+            self.path = scout_path(kwargs.get('game_map'), kwargs.get('locked'), unit.position())
+            self.moving = True
+
+        next_pos = self.path[1]
+        #  kwargs.get('locked')[next_pos[1]][next_pos[0]] = 1
+        # print("next Position: ", next_pos)
+        # print("Path to next pos: ", self.path)
+        return createDirectionMove(unit.id, direction_to(unit, next_pos), MAX_MOVEMENT_SPEED[Units.WORKER])
 
 
 def scout_path(game_map: Map, locked: List[List[int]], pos: Tuple[int, int]) -> List[Tuple[int, int]]:
@@ -685,33 +727,120 @@ def scout_path(game_map: Map, locked: List[List[int]], pos: Tuple[int, int]) -> 
     :param pos: position of Scout unit.
     :return: A path the scout will travel
     """
-    before_flag_path = []
-    after_flag_path = []
+
+    path = []
     resources = game_map.find_all_resources()
-    index = len(resources) - 1
-    while index > 0:
-        closest = min_distance(pos, resources)
-        path = bfs(locked, pos, closest)
-        resources.remove(closest)
-        index -= 1
-    return before_flag_path + after_flag_path
+    bottom_right_resources = [resource for resource in resources if resource[0] >= len(locked[0]) // 2 and resource[1] < len(locked) // 2]
+    top_right_resources = [resource for resource in resources if resource[0] >= len(locked[0]) // 2 and resource[1] >= len(locked) // 2]
+    bottom_left_resources = [resource for resource in resources if resource[0] < len(locked[0]) // 2 and resource[1] < len(locked) // 2]
+    top_left_resources = [resource for resource in resources if resource[0] < len(locked[0]) // 2 and resource[1] >= len(locked) // 2]
+
+    enemy_flag_pos = closest_flag(game_map, pos, Tiles.BASE)
+
+    if enemy_flag_pos[1] >= len(locked) // 2:
+        path.extend(create_semi_path(copy.deepcopy(bottom_right_resources), pos, locked))
+        path.extend(create_semi_path(copy.deepcopy(top_right_resources), pos, locked))
+        curr_pos = path[-1]
+        enemy_flag_pos = closest_flag(game_map, curr_pos, Tiles.BASE)
+        path_to_enemy_flag = bfs(locked, curr_pos, enemy_flag_pos)
+        path.extend(path_to_enemy_flag)
+        path.extend(create_semi_path(copy.deepcopy(top_left_resources), path[-1], locked))
+        path.extend(create_semi_path(copy.deepcopy(bottom_left_resources), path[-1], locked))
+
+    else:
+        path.extend(create_semi_path(copy.deepcopy(top_right_resources), pos, locked))
+        path.extend(create_semi_path(copy.deepcopy(bottom_right_resources), pos, locked))
+
+    # path.extend(create_semi_path(copy.deepcopy(bottom_right_resources), pos, locked))
+    # path.extend(create_semi_path(copy.deepcopy(top_right_resources), pos, locked))
+    #
+    #
+    # print("resources: ", resources)
+    # print("scout_path", path)
+    # curr_pos = path[0]
+    # enemy_flag_pos = closest_flag(game_map, curr_pos, Tiles.BASE)
+    # path_to_enemy_flag = bfs(locked, curr_pos, enemy_flag_pos)
+    #
+    # if path_to_enemy_flag is not None:
+    #     curr_pos = path_to_enemy_flag[-1]
+    #     path.extend(path_to_enemy_flag)
+    #
+    # path.extend(create_semi_path(copy.deepcopy(top_left_resources), pos, locked))
+    # path.extend(create_semi_path(copy.deepcopy(bottom_left_resources), pos, locked))
+
+    # path.extend(create_semi_path(left_resources, curr_pos, locked))
+
+    return path
 
 
-def min_distance(pos1: Tuple[int, int], resources: List[Tuple[int, int]]) -> Tuple[int, int]:
+def create_semi_path(resources: List[Tuple[int, int]],
+                     position: Tuple[int, int], locked: List[List[int]]) -> List[Tuple[int, int]]:
     """
-    Gets the closest resource from the units position.
+    Creates part of the full path for Scout
+    :param locked: Grid with locked positions
+    :param resources: List of resources to visit
+    :param position: Position of Scout
+    :return: Half of the Scouts full path
+    """
+    index = 0
+    path = []
 
-    :param pos1: Position of Unit that will move
+    while index < len(resources) - 1:
+        print("path,", path)
+        if not path:
+            curr_pos = position
+        else:
+            curr_pos = path[-1]
+        print("right resources:", resources)
+        closest = min_distance(curr_pos, resources)
+        print("curr_pos: ", curr_pos, " closest resource: ", closest, "\n")
+        temp_path = bfs(locked, curr_pos, closest)
+        random_stop_point = random.randint(2, 4)
+        resources.remove(closest)
+        if temp_path is not None and len(temp_path) > 2:
+            path.extend([i for i in temp_path[:-random_stop_point] if i not in path])
+
+        elif temp_path is not None:
+            path.extend(temp_path[:-1])
+
+    return path
+
+
+def closest_flag(game_map: Map, position: Tuple[int, int], tileType: Tiles) -> (int, int):
+    """
+    Returns the coordinates of the closest tile to <unit>.
+    """
+    locations = game_map.find_all_tiles_of_type(tileType)
+    c, r = position
+    result = None
+    so_far = 999999
+    for (c_2, r_2) in locations:
+        dc = c_2 - c
+        dr = r_2 - r
+        dist = abs(dc) + abs(dr)
+        if dist < so_far:
+            result = (c_2, r_2)
+            so_far = dist
+    return result
+
+
+def min_distance(pos: Tuple[int, int], resources: List[Tuple[int, int]]) -> Tuple[int, int]:
+    """
+    Gets the closest resource from the units position and returns the position
+
+    :param pos: Position of Unit that will move
     :param resources: List of all the resources on the map
     :return: Returns the position of the closest resource.
     """
 
     closest = 999999
-
+    result = None
     for pos2 in resources:
-        if math.sqrt((pos2[1] - pos1[1]) ** 2 - (pos2[0] - pos1[0]) ** 2) < closest:
-            closest = pos2
-    return closest
+        distance = abs(pos2[0] - pos[0]) + abs(pos2[1] - pos[1])
+        if distance < closest:
+            result = pos2
+            closest = distance
+    return result
 
 
 def closest_resource(avail_resources: dict, unit: Unit) -> Optional[Tuple[int, int]]:
@@ -778,3 +907,30 @@ def is_within_map(map: List[List[int]], x: int, y: int) -> bool:
 # index][1]) + abs(source_combo[0] - target_combo[index][0]) lst.append((source_combo[2], dist, target_combo[index]))
 # (id, distance, target) temp.append(lst) min_combo = min(temp, key=lambda x: sum(i[1] for i in x)) min_dict = {i[0]:
 # i[2] for i in min_combo} return min_dict
+
+
+def get_prime_coordinates(game_map: Map, locked: List[List[int]], your_flag: Tuple[int, int], avail_resources: dict) -> List[Tuple[int, int]]:
+    """
+
+    :param avail_resources:
+    :param game_map: game map
+    :param locked: locked positions grid
+    :param your_flag: coordinate of our flag
+    :return: list of locations for knights to defend
+    """
+    coordinates = []
+    for resource_coord in avail_resources.keys():
+        for i in range(resource_coord[0] - 2, resource_coord[0] + 3):
+            for j in range(resource_coord[1] - 2, resource_coord[1] + 3):
+                if is_within_map(locked, i, j):
+                    if locked[j][i] != 1:
+                        if (i, j) not in avail_resources.keys():
+                            if abs(i - resource_coord[0]) + abs(j - resource_coord[1]) == 3:
+                                coordinates.append((i, j))
+                            elif abs(i - resource_coord[0]) + abs(j - resource_coord[1]) == 2:
+                                if abs(i - resource_coord[0]) == 2 and j == resource_coord[1]:
+                                    coordinates.append((i, j))
+    random.shuffle(coordinates)
+
+    return coordinates
+
